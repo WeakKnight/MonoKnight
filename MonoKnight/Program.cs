cf950dccbb7375184f3ebdc00a7fff5903d02b70
Knight <790184126@qq.com>
2017-05-03 00:35:58 +0800

    fix condition

--- MonoKnight/Program.cs
+++ MonoKnight/Program.cs
@@ -6,19 +6,18 @@ namespace MonoKnight
 {
 	class MainClass
 	{
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
+		[DllImport("kernel32.dll")]
+		static extern IntPtr GetConsoleWindow();
 
-        [DllImport("user32.dll")]
-        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+		[DllImport("user32.dll")]
+		static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-        public static void Main(string[] args)
+        public static void Main()
 		{
-            if (PlatformID.MacOSX != Environment.OSVersion.Platform)
+			if (System.PlatformID.Unix != System.Environment.OSVersion.Platform && System.PlatformID.MacOSX != System.Environment.OSVersion.Platform)
             {
                 const int SW_HIDE = 0;
                 var handle = GetConsoleWindow();
-                // Hide
                 ShowWindow(handle, SW_HIDE);
             }
 

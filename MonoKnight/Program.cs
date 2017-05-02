df2abd5b9b6eb18a448ae931ccb2fe84d22e0382
unknown <>
2017-05-03 00:31:12 +0800

    hide console window

--- MonoKnight/Program.cs
+++ MonoKnight/Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using OpenTK;
+using System.Runtime.InteropServices;
 
 namespace MonoKnight
 {
 	class MainClass
 	{
-		public static void Main(string[] args)
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public static void Main(string[] args)
 		{
-			new Window().Run(60);
+            if (PlatformID.MacOSX != Environment.OSVersion.Platform)
+            {
+                const int SW_HIDE = 0;
+                var handle = GetConsoleWindow();
+                // Hide
+                ShowWindow(handle, SW_HIDE);
+            }
+
+            new Window().Run(60);
 		}
 	}
 }

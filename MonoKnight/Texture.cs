732203e5e5ec98635471fa82f1de9cc8e6462124
unknown <>
2017-05-08 22:26:58 +0800

    fix texture

--- MonoKnight/Texture.cs
+++ MonoKnight/Texture.cs
@@ -3,6 +3,8 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Graphics;
+using StbSharp;
+using System.IO;
 
 namespace MonoKnight
 {
@@ -10,7 +12,32 @@ namespace MonoKnight
 	{
 		public Texture(string texPath)
 		{
-			Bitmap bitmap = new Bitmap(texPath);
+            ImageReader loader = new ImageReader();
+            using (System.IO.Stream stream = File.Open(texPath, FileMode.Open))
+            {
+                StbSharp.Image image = loader.Read(stream, Stb.STBI_rgb_alpha);
+                Tex = GL.GenTexture();
+                GL.BindTexture(TextureTarget.Texture2D, Tex);
+                GL.TexImage2D(TextureTarget.Texture2D,
+                          0,
+                          PixelInternalFormat.Rgba,
+                          image.Width,
+                          image.Height,
+                          0,
+                          OpenTK.Graphics.OpenGL.PixelFormat.Bgra,
+                          PixelType.UnsignedByte,
+                          image.Data);
+                GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+                //GL.TexImage2D(TextureTarget.Texture2D,0,PixelInternalFormat.Rgba,
+                GL.BindTexture(TextureTarget.Texture2D, 0);
+            }
+            //
+            /*
+            Bitmap bitmap = new Bitmap(texPath);
 			Tex = GL.GenTexture();
 			GL.BindTexture(TextureTarget.Texture2D, Tex);
 			BitmapData data = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
@@ -33,6 +60,7 @@ namespace MonoKnight
     		GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int) TextureWrapMode.Repeat);
             //GL.TexImage2D(TextureTarget.Texture2D,0,PixelInternalFormat.Rgba,
             GL.BindTexture(TextureTarget.Texture2D, 0);
+            */
         }
 		public void Bind()
 		{
